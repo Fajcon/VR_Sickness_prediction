724b80cdd91c7470eeabfc918266ec7247e130db
Jakub Ficoń <34249064+Fajcon@users.noreply.github.com>
2024-03-13 12:06:37 +0100

    Simple server

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from csv import writer
 app = FastAPI()
 
 
-@app.get("/test/{seconds}")
-async def test(seconds: int):
-    return {"seconds": seconds}
+class VRSicknessInfo(BaseModel):
+    timestamp: int
+    in_game_second: int
+    id: str
 
 
-@app.post("/vr_sickness/")
-async def save_param():
-    return "OK"
+def save_info_to_file(vr_sickness_info: VRSicknessInfo):
+    line = [vr_sickness_info.timestamp, vr_sickness_info.in_game_second]
+    with open(vr_sickness_info.id + '.csv', 'a', newline='') as f_object:
+        writer_object = writer(f_object)
+        writer_object.writerow(line)
+        f_object.close()
+
+@app.get("/test/{test}")
+async def test(test: int):
+    return {"seconds": test}
+
+
+@app.post("/vr_sickness")
+async def save_param(vr_sickness_info: VRSicknessInfo):
+    print(vr_sickness_info)
+    save_info_to_file(vr_sickness_info)
+    return vr_sickness_info
